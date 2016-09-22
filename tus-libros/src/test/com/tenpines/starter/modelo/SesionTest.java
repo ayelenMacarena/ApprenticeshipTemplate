3cf3bb529aad4b9a824cd232bb2720cd8c136ccf
kevin <kevenvarela@gmail.com>
2016-09-22 19:16:21 -0300

    reparacion de clase TestTransversal, algunos refactors y test de sesion

---
+++ tus-libros/src/test/com/tenpines/starter/modelo/SesionTest.java
@@ -0,0 +1,7 @@
+package com.tenpines.starter.modelo;
+
+/**
+ * Created by Kevin on 22/09/16.
+ */
+public class SesionTest {
+}
--- tus-libros/src/test/com/tenpines/starter/web/TestTransversal.java
+++ tus-libros/src/test/com/tenpines/starter/web/TestTransversal.java
@@ -3,8 +3,10 @@ package com.tenpines.starter.web;
 import com.tenpines.starter.integracion.SpringTestBase;
 import com.tenpines.starter.modelo.Carrito;
 import com.tenpines.starter.modelo.Cliente;
+import com.tenpines.starter.modelo.Libro;
 import com.tenpines.starter.modelo.Sesion;
 import com.tenpines.starter.servicios.ServicioDeCarritos;
+import com.tenpines.starter.servicios.ServicioDeCatalogo;
 import com.tenpines.starter.servicios.ServicioDeCliente;
 import com.tenpines.starter.servicios.ServicioDeSesion;
 import org.assertj.core.api.AssertionsForClassTypes;
@@ -16,7 +18,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
-public class CarritoControllerTest extends SpringTestBase {
+public class TestTransversales extends SpringTestBase {
 
     @Autowired
     private ServicioDeCarritos servicioDeCarritos;
@@ -26,6 +28,9 @@ public class CarritoControllerTest extends SpringTestBase {
 
     @Autowired
     private ServicioDeSesion servicioDeSesion;
+
+    @Autowired
+    private ServicioDeCatalogo servicioDeCatalogo;
 
     @Test
     public void hayCarritoCreado(){
@@ -48,4 +53,16 @@ public class CarritoControllerTest extends SpringTestBase {
         assertFalse(servicioDeCliente.loguearCliente(1L, "1111"));
     }
 
+    @Test
+    public void agregarUnItemAUnCarritoYQueLoContenga() throws Exception {
+        Cliente cliente = Cliente.crearCliente("1234");
+        servicioDeCliente.almacenar(cliente);
+        Sesion sesion = servicioDeSesion.crearCarrito(cliente);
+        Carrito carrito = sesion.getCarrito();
+        Libro libro = servicioDeCatalogo.agregarLibroAlCatalogo("Guerra de los mundos", "123456789", 45);
+
+        servicioDeSesion.agregarLibro(sesion, libro.getId(), 1);
+
+        assertThat((servicioDeSesion.mostrarLibrosDeCarrito(sesion.getId_sesion())).stream().filter(li -> li.equals(libro))).contains(libro);
+    }                //TODO CHECKEAR ESTE EQUALS -- CORRESPONDE A *1
 }
