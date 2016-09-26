5a78a304a0036a879fdd6d8a0c36e06643ba3342
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2016-09-26 15:13:23 -0300

    Cacheo de Error 500, cambiado a 400

---
+++ tus-libros/src/main/java/com/tenpines/starter/web/GlobalExceptionHandlingController.java
@@ -0,0 +1,36 @@
+package com.tenpines.starter.web;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class GlobalExceptionHandlingController extends IOException {
+
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleDefaultException(HttpServletResponse response, Exception exception)
+            throws Exception {
+        // Rethrow annotated exceptions or they will be processed here instead.
+        if (AnnotationUtils.findAnnotation(exception.getClass(),
+                ResponseStatus.class) != null)
+            throw exception;
+
+        String message = exception.getMessage();
+        message = "Error " + HttpStatus.BAD_REQUEST + " - " + message;
+
+        return new ResponseEntity(message,HttpStatus.BAD_REQUEST);
+
+    }
+
+
+
+
+}
